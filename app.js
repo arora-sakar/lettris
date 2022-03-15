32918217636b7c44bc1dd68b7a677d0149732fac
Sakar Arora <arora.sakar@gmail.com>
2022-03-15 16:05:16 +0530

    code clean up

--- app.js
+++ app.js
@@ -1,33 +1,39 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const NumRows = 16;
+  const NumCols = 10;
+  const BackColor = '#5cdb95';
+  const FrontColor = '#41b3a3';
+  const DisplayColor = '#8ee4af';
+  const Unselected = -1;
+  const SelectedSquareBorder = "inset";
+  const UnselectedSquareBorder = "outset";
+
   const WordList = new Array();
   let Score = 0;
   let LetterBox = null;
   let GameOver = false;
-  const NumRows = 16;
-  const NumCols = 10;
   const LetterBoxSize = NumCols;
-  const WordDisplay = document.querySelector('#worddisplay');
-  const grid = document.querySelector('.grid-container');
-  const startBtn = document.querySelector('#start-button');
-  const backBtn = document.querySelector('#back-button');
-  let timerid = null;
-  let square_grid = new Array();
+  const Display = document.querySelector('#display');
+  const StartBtn = document.querySelector('#start-button');
+  const BackBtn = document.querySelector('#back-button');
+  let TimerId = null;
+  let SquareGrid = new Array();
   let SquaresSelected = null;
   const WeightedLetters = "EEEEEEEEEEAAAAAAAARRRRRRRIIIIIIIOOOOOOOTTTTTTTNNNNNNNSSSSSSLLLLLCCCCCUUUUDDDPPPMMMHHHGGBBFFYYWKVXZJQ";
-
+  const grid = document.querySelector('.grid-container');
   for (i = 0; i < NumRows; i++) {
-    square_row = new Array();
+    const square_row = new Array();
     for (j = 0; j < NumCols; j++) {
       let square = document.createElement("div");
       square.addEventListener('click', squareClickAction);
       square.r = i;
       square.c = j;
-      square.selected = -1;
+      square.selected = Unselected;
       square.letter = '';
       square_row.push(square);
       grid.appendChild(square);
     }
-    square_grid.push(square_row);
+    SquareGrid.push(square_row);
   }
 
   async function loadWords(url) {
@@ -43,7 +49,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   function isSquareClickValid(square) {
     //console.log(square.selected);
-    if (GameOver || square.selected >= 0 || square.letter === '' || timerid == null)
+    if (GameOver || square.selected != Unselected || square.letter === '' || TimerId == null)
       return false;
     return true;
   }
@@ -53,38 +59,31 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   function squareClickAction(evt) {
-    //console.log("Square clicked " + this.innerText + " (" + this.r + "," + this.c + ")");
     if (!isSquareClickValid(this))
       return;
-    this.style.border = "inset";
-    this.style.backgroundColor = '#5cdb95';
+    this.style.border = SelectedSquareBorder;
+    this.style.backgroundColor = BackColor;
     if (SquaresSelected == null) {
       SquaresSelected = new Array();
-      WordDisplay.innerText = '';
+      Display.innerText = '';
     }
-    WordDisplay.innerText = WordDisplay.innerText + this.innerText;
+    Display.innerText = Display.innerText + this.innerText;
     this.selected = SquaresSelected.push(this) - 1;
-    if (isValidWord(WordDisplay.innerText)) {
-      WordDisplay.style.border = 'outset';
-      WordDisplay.style.backgroundColor = '#41b3a3';
+    if (isValidWord(Display.innerText)) {
+      Display.style.border = UnselectedSquareBorder;
+      Display.style.backgroundColor = FrontColor;
     }
     else {
-      WordDisplay.style.border = 'inset';
-      WordDisplay.style.backgroundColor = '#8ee4af';
+      Display.style.border = SelectedSquareBorder;
+      Display.style.backgroundColor = DisplayColor;
     }
-  }
-
-  function pickVowel() {
-    if (Math.floor(Math.random() * 5) <= 2)
-      return false;
-    return true;
   }
 
   function createLetterBox() {
     LetterBox = new Array();
     let l = null;
     for (i = 0; i < LetterBoxSize; i++) {
-      square = square_grid[0][i];
+      square = SquareGrid[0][i];
       if (square.innerText === '') {
         idx = Math.floor(Math.random() * WeightedLetters.length);
         square.letter = WeightedLetters[idx];
@@ -96,10 +95,10 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     }
     if (GameOver) {
-      clearInterval(timerid);
+      clearInterval(TimerId);
       LetterBox = null;
       alert("Game Over!!");
-      startBtn.innerText = 'START';
+      StartBtn.innerText = 'START';
     }
   }
 
@@ -109,7 +108,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     for (i = 0; i < LetterBoxSize; i++) {
       if (LetterBox[i].r < (NumRows - 1)) {
-        square = square_grid[LetterBox[i].r+1][i];
+        square = SquareGrid[LetterBox[i].r+1][i];
         if (square.letter === '')
           return false;
       }
@@ -126,13 +125,13 @@ document.addEventListener('DOMContentLoaded', () => {
         continue;
       square.innerText = square.letter;
 
-      if (square.selected >= 0) {
-        square.style.border = "inset";
-        square.style.backgroundColor = '#5cdb95';
+      if (square.selected != Unselected) {
+        square.style.border = SelectedSquareBorder;
+        square.style.backgroundColor = BackColor;
       }
       else {
-        square.style.border = "outset";
-        square.style.backgroundColor = '#41b3a3';
+        square.style.border = UnselectedSquareBorder;
+        square.style.backgroundColor = FrontColor;
       }
     }
   }
@@ -143,8 +142,8 @@ document.addEventListener('DOMContentLoaded', () => {
     for (i = 0; i < LetterBoxSize; i++) {
       square = LetterBox[i];
       square.innerText = '';
-      square.style.border = "none";
-      square.style.backgroundColor = '#5cdb95';
+      square.style.border = '';
+      square.style.backgroundColor = BackColor;
     }
   }
 
@@ -152,7 +151,7 @@ document.addEventListener('DOMContentLoaded', () => {
     if (r >= (NumRows - 1))
       return true;
 
-    if (square_grid[r+1][c].letter === '')
+    if (SquareGrid[r+1][c].letter === '')
       return false;
 
     return true;
@@ -166,12 +165,12 @@ document.addEventListener('DOMContentLoaded', () => {
       for (i = 0; i < LetterBoxSize; i++) {
         if (!isLetterSettled(LetterBox[i].r, LetterBox[i].c)) {
           let current_square = LetterBox[i];
-          let below_square = square_grid[LetterBox[i].r+1][LetterBox[i].c];
+          let below_square = SquareGrid[LetterBox[i].r+1][LetterBox[i].c];
           below_square.letter = current_square.letter;
           current_square.letter = '';
-          if (current_square.selected >= 0) {
+          if (current_square.selected != Unselected) {
             below_square.selected = current_square.selected;
-            current_square.selected = -1;
+            current_square.selected = Unselected;
             SquaresSelected[below_square.selected] = below_square;
           }
           LetterBox[i] = below_square;
@@ -192,36 +191,36 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   function cleanGameState() {
-    for (i = 0; i < square_grid.length; i++) {
-      for (j = 0; j < square_grid[i].length; j++) {
-        square_grid[i][j].selected = -1;
-        square_grid[i][j].letter = '';
-        square_grid[i][j].innerText = '';
-        square_grid[i][j].style.border = 'none';
-        square_grid[i][j].style.backgroundColor = '#5cdb95';
+    for (i = 0; i < SquareGrid.length; i++) {
+      for (j = 0; j < SquareGrid[i].length; j++) {
+        SquareGrid[i][j].selected = Unselected;
+        SquareGrid[i][j].letter = '';
+        SquareGrid[i][j].innerText = '';
+        SquareGrid[i][j].style.border = '';
+        SquareGrid[i][j].style.backgroundColor = BackColor;
       }
     }
     SquaresSelected = null;
-    clearInterval(timerid);
-    timerid = null;
+    clearInterval(TimerId);
+    TimerId = null;
     GameOver = false;
     LetterBox = null;
-    WordDisplay.innerText = '';
+    Display.innerText = '';
     Score = 0;
-    WordDisplay.innerText = "Score: " + Score;
-    startBtn.innerText = 'START';
+    Display.innerText = "Score: " + Score;
+    StartBtn.innerText = 'START';
   }
 
-  startBtn.addEventListener('click', () => {
+  StartBtn.addEventListener('click', () => {
     if (GameOver)
       cleanGameState();
-    if (timerid) {
-      clearInterval(timerid);
-      timerid = null;
-      startBtn.innerText = 'START';
+    if (TimerId) {
+      clearInterval(TimerId);
+      TimerId = null;
+      StartBtn.innerText = 'START';
     } else {
-      timerid = setInterval(gameplay, 1000);
-      startBtn.innerText = 'PAUSE';
+      TimerId = setInterval(gameplay, 1000);
+      StartBtn.innerText = 'PAUSE';
     }
   })
 
@@ -230,49 +229,49 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   function dropUpperSquares(square) {
-    if (square.selected == -1)
+    if (square.selected == Unselected)
       return;
     let r = square.r;
     let c = square.c;
 
-    while (r > 0 && square_grid[r-1][c].innerText != '') {
-      //console.log(square_grid[r-1][c].innerText + ',' + r);
-      if (square_grid[r-1][c].selected >= 0)
-        dropUpperSquares(square_grid[r-1][c]);
-      square_grid[r][c].letter = square_grid[r-1][c].letter;
-      square_grid[r][c].innerText = square_grid[r-1][c].innerText;
-      square_grid[r][c].style.border = square_grid[r-1][c].style.border;
-      square_grid[r][c].style.backgroundColor = square_grid[r-1][c].style.backgroundColor;
+    while (r > 0 && SquareGrid[r-1][c].innerText != '') {
+      //console.log(SquareGrid[r-1][c].innerText + ',' + r);
+      if (SquareGrid[r-1][c].selected != Unselected)
+        dropUpperSquares(SquareGrid[r-1][c]);
+      SquareGrid[r][c].letter = SquareGrid[r-1][c].letter;
+      SquareGrid[r][c].innerText = SquareGrid[r-1][c].innerText;
+      SquareGrid[r][c].style.border = SquareGrid[r-1][c].style.border;
+      SquareGrid[r][c].style.backgroundColor = SquareGrid[r-1][c].style.backgroundColor;
       r--;
     }
-    square_grid[r][c].letter = '';
-    square_grid[r][c].innerText = '';
-    square_grid[r][c].style.border = '';
-    square_grid[r][c].style.backgroundColor = '#5cdb95';
-    square.selected = -1;
+    SquareGrid[r][c].letter = '';
+    SquareGrid[r][c].innerText = '';
+    SquareGrid[r][c].style.border = '';
+    SquareGrid[r][c].style.backgroundColor = BackColor;
+    square.selected = Unselected;
   }
 
-  WordDisplay.addEventListener('click', () => {
+  Display.addEventListener('click', () => {
     if (SquaresSelected == null)
       return;
 
-    if (!isValidWord(WordDisplay.innerText))
+    if (!isValidWord(Display.innerText))
       return;
 
     for (i = 0; i < SquaresSelected.length; i++) {
       dropUpperSquares(SquaresSelected[i]);
     }
-    Score += getWordPoints(WordDisplay.innerText.length);
+    Score += getWordPoints(Display.innerText.length);
     SquaresSelected = null;
-    WordDisplay.innerText = '';
-    WordDisplay.style.backgroundColor = '#8ee4af';
-    WordDisplay.style.border = 'inset';
-    WordDisplay.innerText = "Score: " + Score;
+    Display.innerText = '';
+    Display.style.backgroundColor = DisplayColor;
+    Display.style.border = SelectedSquareBorder;
+    Display.innerText = "Score: " + Score;
   })
 
   //assign functions to keyCodes
   function control(e) {
-    if (timerid == null)
+    if (TimerId == null)
       return;
     if (e.keyCode === 40) {
       moveLetterBoxDown();
@@ -280,27 +279,27 @@ document.addEventListener('DOMContentLoaded', () => {
   }
   document.addEventListener('keyup', control)
 
-  backBtn.addEventListener('click', () => {
+  BackBtn.addEventListener('click', () => {
     if (SquaresSelected == null)
       return;
     square = SquaresSelected.pop();
-    if (square && square.selected >= 0) {
-      square.style.border = "outset";
-      square.style.backgroundColor = '#41b3a3';
-      square.selected = -1;
-      let word = WordDisplay.innerText;
+    if (square && square.selected != Unselected) {
+      square.style.border = UnselectedSquareBorder;
+      square.style.backgroundColor = FrontColor;
+      square.selected = Unselected;
+      let word = Display.innerText;
       let newWord = '';
       for (i = 0; i < word.length - 1; i++) {
         newWord = newWord + word[i];
       }
-      WordDisplay.innerText = newWord;
-      if (isValidWord(WordDisplay.innerText)) {
-        WordDisplay.style.border = 'outset';
-        WordDisplay.style.backgroundColor = '#41b3a3';
+      Display.innerText = newWord;
+      if (isValidWord(Display.innerText)) {
+        Display.style.border = UnselectedSquareBorder;
+        Display.style.backgroundColor = FrontColor;
       }
       else {
-        WordDisplay.style.border = 'inset';
-        WordDisplay.style.backgroundColor = '#8ee4af';
+        Display.style.border = SelectedSquareBorder;
+        Display.style.backgroundColor = DisplayColor;
       }
     }
   })
